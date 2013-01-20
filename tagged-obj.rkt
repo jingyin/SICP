01c9d8a3a1e5d3ce8fae5f59fc7a2384c8e06f81
Jingyi Ni <colonel.ni.jingyi@gmail.com>
2013-01-20 01:10:21 -0800

    generic package for handling primitive/rational/complex arithmetic, in their own package

--- tagged-obj.rkt
+++ tagged-obj.rkt
@@ -3,14 +3,16 @@
 (provide attach-tag type-tag contents)
 
 (define (attach-tag type-tag contents)
-  (cons type-tag contents))
+  (if (number? contents)
+      contents
+      (cons type-tag contents)))
 
 (define (type-tag datum)
-  (if (pair? datum)
-      (car datum)
-      (error "Bad tagged datum - TYPE-TAG" datum)))
+  (cond ((pair? datum) (car datum))
+        ((number? datum) 'scheme-number)
+        (else (error "Bad tagged datum - TYPE-TAG" datum))))
 
 (define (contents datum)
-  (if (pair? datum)
-      (cdr datum)
-      (error "Bad tagged datum - CONTENTS" datum)))
+  (cond ((pair? datum) (cdr datum))
+        ((number? datum) datum)
+        (else (error "Bad tagged datum - CONTENTS" datum))))
