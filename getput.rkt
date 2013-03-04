b9d9f7e8a20edf4b54a8fd6ee82d9fdd636414b6
Jingyi Ni <colonel.ni.jingyi@gmail.com>
2013-03-03 21:58:57 -0800

    reformat

--- getput.rkt
+++ getput.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(provide put get)
+(provide put get global-array)
+
+(require racket/trace)
 
 (define global-array '())
 
@@ -10,15 +12,14 @@
 
 (define (put op type item)
   (define (put-helper k array)
-      (cond ((null? array) (list(make-entry k item)))
-                ((equal? (key (car array)) k) array)
-                          (else (cons (car array) (put-helper k (cdr array))))))
-    (set! global-array (put-helper (list op type) global-array)))
+    (cond ((null? array) (list(make-entry k item)))
+          ((equal? (key (car array)) k) array)
+          (else (cons (car array) (put-helper k (cdr array))))))
+  (set! global-array (put-helper (list op type) global-array)))
 
 (define (get op type)
   (define (get-helper k array)
-      (cond ((null? array) #f)
-                ((equal? (key (car array)) k) (value (car array)))
-                          (else (get-helper k (cdr array)))))
-    (get-helper (list op type) global-array))
-
+    (cond ((null? array) #f)
+          ((equal? (key (car array)) k) (value (car array)))
+          (else (get-helper k (cdr array)))))
+  (get-helper (list op type) global-array))
