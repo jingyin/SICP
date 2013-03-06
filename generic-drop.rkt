6a5c609eb87fde68f657b279621b4ddb314299b4
Jingyi Ni <colonel.ni.jingyi@gmail.com>
2013-03-05 22:54:11 -0800

    ex 2.85: implemented generic project operation, and drop; didn't add drop to apply-generic because doing so would require apply-generic knowing what operations not to drop

---
+++ generic-drop.rkt
@@ -0,0 +1,54 @@
+#lang racket
+
+(require "tagged-obj-original.rkt")
+(require "getput.rkt")
+(require "func-util.rkt")
+(require racket/trace)
+
+(provide apply-generic drop)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-raise a1 a2))
+                      (t2->t1 (get-raise a2 a1)))
+                  (cond (t1->t2 (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1 (apply-generic op a1 (t2->t1 a2)))
+                        (else error "No method for these types" (list op type-tags)))))
+              (error "No method for these types - APPLY-GENERIC" (list op type-tags)))))))
+
+; return a function that raises a1 to a2
+; if such a coercion is not possible, return false
+(define (get-raise a1 a2)
+  (define (identity x) x)
+  (define (get-coercion-rec a1 a2 acc)
+    (if a1
+        (if (eq? (type-tag a1) (type-tag a2))
+            acc
+            (let ((raise-f (get 'raise (list (type-tag a1)))))
+              (if raise-f
+                  ; would be easier if we can just use raise-f as is (no stripping of type tags)
+                  (get-coercion-rec (raise-f (contents a1)) a2 (compose (compose raise-f contents) acc))
+                  #f)))
+        #f))
+  ;(trace get-coercion-rec)
+  (get-coercion-rec a1 a2 identity))
+
+; drops a given object to the lowest level in the hierarchy without losing information
+; repeatedly project an object down, and verify raise gives back the same object
+(define (drop x)
+  (define (raise x) (apply-generic 'raise x))
+  (define (equ? x y) (apply-generic 'equ? x y))
+  (define (project x) (apply-generic 'project x))
+  (let ((projected (project x)))
+    (if projected
+        (let ((raised (raise projected)))
+          (if (equ? raised x)
+              (drop projected)
+              x))
+        x)))
