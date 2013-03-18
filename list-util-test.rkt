9d0f3ac7e376cad3fc7892d4e7bb58347595890a
Jingyi Ni <colonel.ni.jingyi@gmail.com>
2013-03-17 22:25:47 -0700

    new list util functions: zip/generate/or-list

--- list-util-test.rkt
+++ list-util-test.rkt
@@ -12,3 +12,12 @@
 (check-true (and-list '()))
 (check-true (and-list '(#t #t #t)))
 (check-false (and-list '(#f #t #t)))
+
+(check-false (or-list '()))
+(check-true (or-list '(#t #f #f)))
+(check-false (or-list '(#f #f #f)))
+
+(check-true (equal? '() (zip '() '() '())))
+(check-true (equal? '((1 2 3) (4 5 6) (7 8 9)) (zip '(1 4 7) '(2 5 8) '(3 6 9))))
+
+(check-true (equal? '(1 1 1) (generate 1 3)))
--- list-util.rkt
+++ list-util.rkt
@@ -1,8 +1,9 @@
 #lang racket
 
 (require "accumulate.rkt")
+(require racket/trace)
 
-(provide enumerate-interval flatmap list-identical? and-list)
+(provide enumerate-interval flatmap list-identical? and-list or-list zip generate)
 
 (define (enumerate-interval low high)
   (if (> low high)
@@ -16,8 +17,14 @@
 ; a logical-and implementation that works for lists
 (define (and-list l)
   (if (null? l)
-      true
+      #t
       (and (car l) (and-list (cdr l)))))
+
+; a logical-or implementation that works for lists
+(define (or-list l)
+  (if (null? l)
+      #f
+      (or (car l) (or-list (cdr l)))))
 
 (define (list-identical? l)
   (define (enumerate x n)
@@ -27,3 +34,17 @@
   (if (null? l)
       true
       (equal? l (enumerate (car l) (length l)))))
+
+(define (zip . args)
+  (let ((is-null? (map null? args)))
+    (if (or-list is-null?)
+        (if (and-list is-null?)
+            '()
+            (error "lists of unequal length - ZIP"))
+        (cons (map car args)
+              (apply zip (map cdr args))))))
+
+(define (generate t n)
+  (if (= n 0)
+      '()
+      (cons t (generate t (- n 1)))))
